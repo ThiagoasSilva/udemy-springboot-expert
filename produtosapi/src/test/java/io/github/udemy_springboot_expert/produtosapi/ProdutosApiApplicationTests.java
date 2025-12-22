65e3dbbae81a93f493ba8f6138e1e0147882a96a
thiagoassilva <thiagoantonio2108@gmail.com>
2025-12-22 11:00:43 -0300

    projeto inicial

---
+++ produtosapi/src/test/java/io/github/udemy_springboot_expert/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.udemy_springboot_expert.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
